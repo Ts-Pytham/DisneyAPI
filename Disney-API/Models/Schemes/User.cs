b60b8831aabe844e57806d3192e48edad1684187
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-04-12 13:27:52 -0500

    Mejora en los DataAnnotations de la clase User

--- Disney-API/Models/Schemes/User.cs
+++ Disney-API/Models/Schemes/User.cs
@@ -5,7 +5,9 @@ namespace Disney_API.Models.Schemes
 {
     public class User : IUser
     {
+        [EmailAddress(ErrorMessage = "El correo no es válido, comprueba si lo escribiste bien.")]
         [Required(ErrorMessage = "El usuario es obligatorio.")]
+        [StringLength(50, ErrorMessage = "El correo debe de tener como máximo 50 caracteres.")]
         public string Email { get; set; } = null!;
         [Required(ErrorMessage = "La clave es obligatoria.")]
         public string Password { get; set; } = null!;
